193126eca68299bbb9aaccb21f345378db799057
NL0A4EYII5 <139157800+HarveyIvan-Chan@users.noreply.github.com>
2024-03-15 12:11:38 +0800

    Update indexRouter.js

--- src/routes/indexRouter.js
+++ src/routes/indexRouter.js
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import userPostRouter from './userPostRouter.js';
+import userCommentRouter from './commentRouter.js';
 
 const router = Router();
 console.log("index");
@@ -12,6 +13,11 @@ router.get("/homepage", (req, res) => {
     res.redirect("/home");
 });
 
+router.get("/login", (req, res) => {
+    res.render("login");
+});
+
 router.use(userPostRouter);
+router.use(userCommentRouter);
 
 export default router;
