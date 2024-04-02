8f87a5b5e276370779aaa0e645cfb5df72c14a8c
belzooper78 <139192787+belzooper78@users.noreply.github.com>
2024-04-02 20:15:52 +0800

    working on populate for name and picture

--- src/routes/userPostRouter.js
+++ src/routes/userPostRouter.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
+import { Types } from 'mongoose';
 import user_posts from '../db/user_post.js';
+import user_Account from '../db/user.js';
 
 const userPostRouter = Router();
 
@@ -18,9 +20,22 @@ userPostRouter.post("/userPosts", async (req, res) => {
     console.log("POST request received for /home");
     // console.log(req.body)
     try {
-        const newUser_Post = new user_posts(req.body);
+        //const newUser_Post = new user_posts(req.body);
+        const user = req.session.user;
+        const userId= new Types.ObjectId(user);
+        const datenow = new Date();
+        const currentDATE = datenow.getDate()+"/"+datenow.getMonth()+"/"+datenow.getFullYear();
+       
+      
+        const newUser_Post = new user_posts({
+            ...req.body,
+            createdBy: userId,
+            createdOn: currentDATE});
         await newUser_Post.validate();
+        await newUser_Post.populate('createdBy');
         await newUser_Post.save();
+        //console.log(JSON.stringify(createdBy.username)+": TESTING");
+
         
         // console.log(newUser_Post);
         res.sendStatus(200);
