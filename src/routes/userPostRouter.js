4af115357bf5f947c2249dea1a3f6d7925e1ca1b
belzooper78 <139192787+belzooper78@users.noreply.github.com>
2024-04-03 04:18:16 +0800

    populate problem fixed

--- src/routes/userPostRouter.js
+++ src/routes/userPostRouter.js
@@ -8,11 +8,19 @@ const userPostRouter = Router();
 
 userPostRouter.get(("/userPosts" || "/home"), async (req, res) => {
     console.log("GET");
-    const user_postsArray = await user_posts.find({}).lean().exec();
+    const user_postsArray = await user_posts.find({}).populate('createdBy').lean().exec();
+
+   // const pfpData = this.createdBy.imageP.data.toString('base64');
+    //const contentType = this.createdBy.imageP.contentType;
+    //await user_postsArray.populate('createdBy').execPopulate();
+    
     res.render("index", {
         title: "UserPosts",
-        userPosts: user_postsArray
+        userPosts: user_postsArray,
+       // pfpData: pfpData,
+       // contentType: contentType   
     });
+    
 });
 // console.log("userpost");
 
@@ -25,20 +33,28 @@ userPostRouter.post("/userPosts", async (req, res) => {
         const userId= new Types.ObjectId(user);
         const datenow = new Date();
         const currentDATE = datenow.getDate()+"/"+datenow.getMonth()+"/"+datenow.getFullYear();
+        
+        
        
-      
         const newUser_Post = new user_posts({
             ...req.body,
             createdBy: userId,
             createdOn: currentDATE});
+        
+            
         await newUser_Post.validate();
-        await newUser_Post.populate('createdBy');
         await newUser_Post.save();
+        
+        //await user_postsArray.find({createdBy : userId }).populate('createdBy').execPopulate();
+
+
+        //await newUser_Post.populate('createdBy');
         //console.log(JSON.stringify(createdBy.username)+": TESTING");
 
         
         // console.log(newUser_Post);
         res.sendStatus(200);
+        
 
     } catch (err) {
         console.error(err);
